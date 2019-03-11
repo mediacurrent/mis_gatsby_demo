dbc73f4ce874539b752556d979be861ef6e9959b
Mark Casias <mark.casias@mediacurrent.com>
2019-03-11 09:21:46 -0600

    GU-9: Updating pacakges and adding Headroom

--- src/components/layouts/Header/index.js
+++ src/components/layouts/Header/index.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import React from "react";
+import Headroom from 'react-headroom';
 
 import Icon from '../../fields/Icon';
 import MainMenu from '../MainMenu';
@@ -7,26 +8,28 @@ import MainMenu from '../MainMenu';
 import './style.scss';
 
 const Header = ({ menuLinks }) => (
-  <header className="section header site-header">
-    <div className="section__container header__container" data-path="" >
-      <div className="header__brand">
-        <a className="header__logo-link" href="/">
-          <Icon name="icon-mc-logo" className="icon icon-mc-logo" />
-        </a>
-      </div>
-      <button className="header__nav-toggle" aria-expanded="false" aria-controls="main-nav-wrapper">
-        <span className="header__nav-toggle-bar"></span>
-        <span className="header__nav-toggle-bar"></span>
-        <span className="header__nav-toggle-bar"></span>
-        <span className="header__nav-toggle-text">Menu</span>
-      </button>
-      <div id="main-nav-wrapper" className="header__nav-wrapper">
-        <div className="header__main-menu">
-          <MainMenu menuLinks={menuLinks} />
+  <Headroom>
+    <header className="section header site-header">
+      <div className="section__container header__container" data-path="" >
+        <div className="header__brand">
+          <a className="header__logo-link" href="/">
+            <Icon name="icon-mc-logo" className="icon icon-mc-logo" />
+          </a>
+        </div>
+        <button className="header__nav-toggle" aria-expanded="false" aria-controls="main-nav-wrapper">
+          <span className="header__nav-toggle-bar"></span>
+          <span className="header__nav-toggle-bar"></span>
+          <span className="header__nav-toggle-bar"></span>
+          <span className="header__nav-toggle-text">Menu</span>
+        </button>
+        <div id="main-nav-wrapper" className="header__nav-wrapper">
+          <div className="header__main-menu">
+            <MainMenu menuLinks={menuLinks} />
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </Headroom>
 );
 
 Header.propTypes = {
