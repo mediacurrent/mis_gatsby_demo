ced055ed1ad5ad1cf2959da89e68b0c849d2e8da
Mark Casias <mark.casias@mediacurrent.com>
2019-03-18 16:18:57 -0600

    GU-13: Updating mainfest and adding metatag helmet

--- src/templates/NodePageTemplate/index.js
+++ src/templates/NodePageTemplate/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 
 // Layout
 import Layout from '../../components/layouts/Layout';
@@ -9,11 +10,14 @@ import Content from '../../components/fields/Content';
 import Heading from '../../components/fields/Heading';
 
 const NodePageTemplate = ({ data }) => {
-  const { title } = data.nodePage;
+  const { title, summary } = data.nodePage;
   const { content } = data.nodePage.r;
   return (
     <Layout>
-      <Heading level={1}>{title}</Heading>
+      <Helmet>
+        <title>{title} | Mediacurrent</title>
+        <meta name="description" content={summary} />
+      </Helmet>
       <Content content={content} />
     </Layout>
   )
