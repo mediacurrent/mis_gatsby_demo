fe9d7e7b817c1e77d4397efddd07393215644f4e
Brooklynn Moor <brooklynn.moor@mediacurrent.com>
2019-04-01 17:24:01 -0400

    GU-23 - WIP, need to adjust the container shadow to be responsive

--- src/components/fields/Contact/index.js
+++ src/components/fields/Contact/index.js
@@ -4,7 +4,7 @@ import Button from '../Button';
 
 import './style.scss';
 
-const Contact = () => {
+const Contact = (props) => {
 
   const formSubmit = (e) => {
     e.preventDefault();
@@ -15,79 +15,83 @@ const Contact = () => {
     <section className="contact-us section breaker">
       <div className="section__container breaker__content">
         <h2 className="title breaker__title">Contact Us</h2>
-        <div className="contact-us__contact-items">
-          <div className="contact-us__items">
-            <h3>What are your challenges?</h3>
-            <p>We can show you how RAIN and other open source tools can increase efficiency within your digital ecosystem</p>
-          </div>
-          <div className="contact-us__items">
-            <form onSubmit={formSubmit} className="form">
-              <div className="form-item form-item--text">
-                <input
-                  id="first_name"
-                  type="text"
-                  className="form-item__input js-form-item"
-                  aria-describedby="required-message"
-                  required
-                />
-                <label htmlFor="first_name" className="form-item__label required-field">
-                  First Name
-                </label>
-                <div id="required-message" className="visually-hidden">
-                  This field is required.
+        <div className="contact__content-wrapper">
+          <div className="contact-us__contact-items">
+            <div className="contact-us__items">
+              <h4>What are your challenges?</h4>
+              <p className="body-text">We can show you how RAIN and other open source tools can increase efficiency within your digital ecosystem</p>
+            </div>
+            <div className="contact-us__items">
+              <form onSubmit={formSubmit} className="form">
+                <div className="form-item form-item--text">
+                  <input
+                    id="first_name"
+                    type="text"
+                    className="form-item__input js-form-item"
+                    aria-describedby="required-message"
+                    required
+                  />
+                  <label htmlFor="first_name" className="form-item__label required-field">
+                    First Name
+                  </label>
+                  <div id="required-message" className="visually-hidden">
+                    This field is required.
+                  </div>
                 </div>
-              </div>
-              <div className="form-item form-item--text">
-                <input
-                  id="last_name"
-                  type="text"
-                  className="form-item__input js-form-item"
-                  aria-describedby="required-message"
-                  required
-                />
-                <label htmlFor="last_name" className="form-item__label required-field">
-                  Last Name
-                </label>
-                <div id="required-message" className="visually-hidden">
-                  This field is required.
+                <div className="form-item form-item--text">
+                  <input
+                    id="last_name"
+                    type="text"
+                    className="form-item__input js-form-item"
+                    aria-describedby="required-message"
+                    required
+                  />
+                  <label htmlFor="last_name" className="form-item__label required-field">
+                    Last Name
+                  </label>
+                  <div id="required-message" className="visually-hidden">
+                    This field is required.
+                  </div>
                 </div>
-              </div>
-              <div className="form-item form-item--email">
-                <input
-                  id="email"
-                  type="text"
-                  className="form-item__input js-form-item"
-                  aria-describedby="required-message"
-                  required
-                />
-                <label htmlFor="email" className="form-item__label required-field">
-                  Email
-                </label>
-                <div id="required-message" className="visually-hidden">
-                  This field is required.
+                <div className="form-item form-item--email">
+                  <input
+                    id="email"
+                    type="text"
+                    className="form-item__input js-form-item"
+                    aria-describedby="required-message"
+                    required
+                  />
+                  <label htmlFor="email" className="form-item__label required-field">
+                    Email
+                  </label>
+                  <div id="required-message" className="visually-hidden">
+                    This field is required.
+                  </div>
                 </div>
-              </div>
-              <div className="form-item form-item--text">
-                <input
-                  id="subject"
-                  type="text"
-                  className="form-item__input js-form-item"
-                  aria-describedby="required-message"
-                  required
-                />
-                <label htmlFor="subject" className="form-item__label required-field">
-                  Subject
-                </label>
-                <div id="required-message" className="visually-hidden">
-                  This field is required.
+                <div className="form-item form-item--text">
+                  <input
+                    id="subject"
+                    type="text"
+                    className="form-item__input js-form-item"
+                    aria-describedby="required-message"
+                    required
+                  />
+                  <label htmlFor="subject" className="form-item__label required-field">
+                    Subject
+                  </label>
+                  <div id="required-message" className="visually-hidden">
+                    This field is required.
+                  </div>
                 </div>
-              </div>
-              <div className="form-item form-item--textarea">
-                <textarea className="form-item__input js-form-item" id="message" name="messagetext"></textarea>
-                <label htmlFor="message" className="form-item__label required-field">Message</label>
-              </div>
-              <Button title="Connect with Us" />
-            </form>
+                <div className="form-item form-item--textarea">
+                  <textarea className="form-item__input js-form-item" id="message" name="messagetext"></textarea>
+                  <label htmlFor="message" className="form-item__label required-field">Message</label>
+                </div>
+                <div className="contact-button-submit">
+                <Button {...props} classes="card__link" title="Connect With Us" />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
